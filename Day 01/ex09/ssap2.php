642f5329942af8e33bbca9c3b4e520d85141b10a
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-10-09 17:08:24 +0200

    Getting there

--- Day 01/ex09/ssap2.php
+++ Day 01/ex09/ssap2.php
@@ -1,12 +1,22 @@
 #!/usr/bin/php
 <?php
+    function    my_sort($x, $y)
+    {
+        $i = 0;
+        $a = strtolower($x);
+        $b = strtolower($y);
+        $order = "abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        while (($i < strlen($x)) || ($))
+    }
     $i = 0;
     if ($argc == 1)
         exit;
-    $in = implode(" ", $argv);
+    
+    $unsorted = join_args($argv, $argc);
+    $unsorted = trim_string($unsorted);
     $out = explode(" ", $in);
-    unset($out[0]);
-    natsort($out);
+    // unset($out[0]);
+    usort($out, "my_sort");
     while ($out[$i] != NULL)
     {
         echo($out[$i++]);
