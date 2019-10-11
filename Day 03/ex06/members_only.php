8fa81bed3985ee8c03a52d8017f6e1afe2e468f4
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-10-11 16:01:18 +0200

    Day03 ex06 done

---
+++ Day 03/ex06/members_only.php
@@ -0,0 +1,13 @@
+<?php
+    if (($_SERVER['PHP_AUTH_USER'] !== "zaz") || ($_SERVER['PHP_AUTH_PW']) !== "Ilovemylittleponey")
+    {
+        header('HTTP/1.0 401 Unauthorized');
+        header('WWW-Authenticate: Basic realm=\'\'Members area\'\'');
+        echo "<html><body>That area is accesable for menbers only</body><html>\n";
+    }
+    else
+    {
+        file_get_contents('../img/42.png');
+        echo "<html><body>Welcome Zaz<br />\n<img src='data:image/png;base64,".base64_encode($file)."'>\n</body></html>\n"; 
+    }
+?>
